27fdcfae04c0b6e73233c93cb1d7803b054f42bc
Andreas Marr <andreasm@artcom.de>
2014-04-07 17:52:45 +0200

    reformatting...

--- src/test/java/com/hoccer/talk/ITSingleClient.java
+++ src/test/java/com/hoccer/talk/ITSingleClient.java
@@ -1,15 +1,18 @@
 package com.hoccer.talk;
 
 // import junit stuff
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
 import static org.junit.Assert.*;
 
 // Utility classes for async tests
 import com.google.code.tempusfugit.temporal.*;
+
 import static com.google.code.tempusfugit.temporal.Duration.seconds;
 import static com.google.code.tempusfugit.temporal.WaitFor.waitOrTimeout;
 
@@ -32,32 +35,32 @@ public class ITSingleClient extends IntegrationTest {
         firstServer.shutdown();
     }
 
-  @Test
-  public void clientConnectAndDisconnectTest() throws Exception {
-      // create client
-      final XoClient c = createTalkClient(firstServer);
+    @Test
+    public void clientConnectAndDisconnectTest() throws Exception {
+        // create client
+        final XoClient c = createTalkClient(firstServer);
 
-      // test waking and connecting
-      assertFalse(c.isAwake());
-      c.wake();
-      assertTrue(c.isAwake());
+        // test waking and connecting
+        assertFalse(c.isAwake());
+        c.wake();
+        assertTrue(c.isAwake());
 
-      waitOrTimeout(new Condition() {
-          @Override
-          public boolean isSatisfied() {
-              return XoClient.STATE_ACTIVE == c.getState();
-          }
-      }, Timeout.timeout(seconds(2)));
+        waitOrTimeout(new Condition() {
+            @Override
+            public boolean isSatisfied() {
+                return XoClient.STATE_ACTIVE == c.getState();
+            }
+        }, Timeout.timeout(seconds(2)));
 
-      // test disconnecting
-      c.deactivate();
-      waitOrTimeout(new Condition() {
-          @Override
-          public boolean isSatisfied() {
-              return XoClient.STATE_INACTIVE == c.getState();
-          }
-      }, Timeout.timeout(seconds(2)));
-  }
+        // test disconnecting
+        c.deactivate();
+        waitOrTimeout(new Condition() {
+            @Override
+            public boolean isSatisfied() {
+                return XoClient.STATE_INACTIVE == c.getState();
+            }
+        }, Timeout.timeout(seconds(2)));
+    }
 
 }
 
