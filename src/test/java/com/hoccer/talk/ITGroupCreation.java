b092c08bec48b24204f1e771f562be9ffd7f5c4c
Andreas Marr <andreasm@artcom.de>
2014-04-17 11:52:37 +0200

    using convenience client methods.

--- src/test/java/com/hoccer/talk/ITGroupCreation.java
+++ src/test/java/com/hoccer/talk/ITGroupCreation.java
@@ -11,32 +11,34 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import static com.jayway.awaitility.Awaitility.*;
-import static org.junit.Assert.*;
-import static org.hamcrest.Matchers.*;
+import java.util.HashMap;
+
+import static com.jayway.awaitility.Awaitility.await;
+import static com.jayway.awaitility.Awaitility.to;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(JUnit4.class)
 public class ITGroupCreation extends IntegrationTest {
 
     private TestTalkServer firstServer;
+    private HashMap<String, XoClient> clients;
 
     @Before
     public void setUp() throws Exception {
         firstServer = createTalkServer();
+        clients = initializeTalkClients(firstServer, 1);
     }
 
     @After
     public void tearDown() throws Exception {
         firstServer.shutdown();
+        shutdownClients(clients);
     }
 
     @Test
     public void createGroupTest() throws Exception {
-        // create client
-        final XoClient client = createTalkClient(firstServer);
-        client.wake();
-
-        await("client is active").untilCall(to(client).getState(), equalTo(XoClient.STATE_ACTIVE));
+        XoClient client = clients.get("client1");
 
         /* TODO: ideally this new group and presence creation stuff and eventually calling createGroup should be more graceful in the clients and disappear form this test entirely */
         TalkClientContact newGroup = TalkClientContact.createGroupContact();
@@ -50,10 +52,6 @@ public class ITGroupCreation extends IntegrationTest {
         await("client knows about created group").untilCall(to(client.getDatabase()).findContactByGroupTag(groupTag), notNullValue());
         final String groupId = client.getDatabase().findContactByGroupTag(groupTag).getGroupId();
         assertNotNull(groupId);
-
-        // test disconnecting
-        client.deactivate();
-        await("client is inactive").untilCall(to(client).getState(), equalTo(XoClient.STATE_INACTIVE));
     }
 
 }
